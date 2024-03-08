1284ed796c9a443b8504f2f984a339e333dc494a
Slumhunden <kroghbastin@gmail.com>
2024-03-08 10:55:52 +0100

    errorhandling

--- src/recipes/RecipeForm.tsx
+++ src/recipes/RecipeForm.tsx
@@ -33,11 +33,11 @@ export default function RecipeForm() {
     }));
   };
   const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // e.preventDefault();
-    // if (formData.id) {
-    //   deleteRecipe(Number(formData.id));
-    //   setFormData({ ...EMPTY_RECIPE });
-    // }
+    e.preventDefault();
+    if (formData.id) {
+      deleteRecipe(Number(formData.id));
+      setFormData({ ...EMPTY_RECIPE });
+    }
   };
 
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
